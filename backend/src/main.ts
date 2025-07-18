de7f2dcf413317e3fb2b37fa13fa9d54f9c8c352
Maria J <mahjuaraujo@gmail.com>
2025-07-18 14:55:42 -0300

    update Dockerfile prod

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -7,29 +6,6 @@ import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.use((req, res, next) => {
-    res.header(
-      'Access-Control-Allow-Origin',
-      'https://brain-agriculture-gilt.vercel.app',
-    );
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET, POST, PATCH, DELETE, OPTIONS',
-    );
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-    );
-    res.header('Access-Control-Allow-Credentials', 'true');
-
-    if (req.method === 'OPTIONS') {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      return res.sendStatus(200);
-    }
-
-    next();
-  });
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AllExceptionsFilter());
   app.use(helmet());
